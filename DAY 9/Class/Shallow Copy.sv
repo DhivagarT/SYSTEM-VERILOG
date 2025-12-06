c0eef390d607242636469da45fb166533ad71756
DHIVAGAR.T <dhivagardhanavel07@gmail.com>
2025-12-06 23:38:46 +0530

    Create Shallow Copy.sv

---
+++ DAY 9/Class/Shallow Copy.sv
@@ -0,0 +1,40 @@
+//Shallow Copy
+class base;
+  int a;
+  int b;
+  
+  function new();
+    a=1;
+    b=2;
+  endfunction
+  
+  function void display();
+    $display("a=%0d B=%0d",a,b);
+  endfunction
+endclass
+
+module exp();
+  base x;
+  base y;
+  initial begin
+    x=new();
+    y=new x;  //Key word
+   
+    $display("BEFORE");
+    x.display();
+    y.display();
+    y.a=3;
+    y.b=4;
+    $display("AFTER");
+    x.display();
+    y.display();
+  end
+endmodule
+
+// OUTPUT
+// # KERNEL: BEFORE
+// # KERNEL: a=1 B=2
+// # KERNEL: a=1 B=2
+// # KERNEL: AFTER
+// # KERNEL: a=1 B=2
+// # KERNEL: a=3 B=4
