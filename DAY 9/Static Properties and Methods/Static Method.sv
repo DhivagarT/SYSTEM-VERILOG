ce8b1112983572397ed769bc6ae38bd64183b6bd
DHIVAGAR.T <dhivagardhanavel07@gmail.com>
2025-12-08 11:23:06 +0530

    Add static methods for setting and displaying values

---
+++ DAY 9/Static Properties and Methods/Static Method.sv
@@ -0,0 +1,31 @@
+//static Methods
+class demo;
+  static string a;
+  static int b;
+  int c;
+  
+  //Static Method to set Value
+  static function void setf1(string val_a,int val_b);
+    a=val_a;
+    b=val_b;
+//  c=2; not declared inside function because it is non-static  
+  endfunction
+  
+  //Static Method to Display Value
+  static function void getf1();
+    $display("A=%0s B=%0d",a,b);
+  endfunction
+endclass
+  
+  module exp;
+    initial begin   //no handle and object created because static depends on class
+      demo::setf1("TOTAL BOX",2);  //demo::setf1 - Keyword
+      demo::getf1();
+      demo::setf1("TOTAL ELEMENT",1);
+      demo::getf1();
+    end
+  endmodule
+
+// OUTPUT
+// # KERNEL: A=TOTAL BOX B=2
+// # KERNEL: A=TOTAL ELEMENT B=1
